ecc5c96824d679317fa12d5298c06690adadf970
Raymond L Maple <rmaple@neumont.edu>
2024-07-12 09:05:58 -0600

    color

--- Engine/Source/Color.h
+++ Engine/Source/Color.h
@@ -17,27 +17,26 @@ struct Color
 	float  operator [] (unsigned int index) const { return (&r)[index]; }
 	float& operator [] (unsigned int index) { return (&r)[index]; }
 
-	//Color operator + (const Color& c) const { return Color{ r + c.r, g + c.g, b + c.b, a + c.a }; }
-	//Color operator - (const Color& c) const { return Color{ r - c.r, g - c.g, b - c.b, a - c.a }; }
-	//Color operator * (const Color& c) const { return Color{ r * c.r, g * c.g, b * c.b, a * c.a }; }
-	//Color operator / (const Color& c) const { return Color{ r / c.r, g / c.g, b / c.b, a / c.a }; }
+	Color operator + (const Color& c) const { return Color{ r + c.r, g + c.g, b + c.b, a + c.a }; }
+	Color operator - (const Color& c) const { return Color{ r - c.r, g - c.g, b - c.b, a - c.a }; }
+	Color operator * (const Color& c) const { return Color{ r * c.r, g * c.g, b * c.b, a * c.a }; }
+	Color operator / (const Color& c) const { return Color{ r / c.r, g / c.g, b / c.b, a / c.a }; }
 													 
-	//Color operator + (float s) const { return Color{ x + s, y + s }; }
-	//Color operator - (float s) const { return Color{ x - s, y - s }; }
-	//Color operator * (float s) const { return Color{ x * s, y * s }; }
-	//Color operator / (float s) const { return Color{ x / s, y / s }; }
-	//
-	//Color& operator += (const Color& c) { x += v.x; y += v.y; return *this; }
-	//Color& operator -= (const Color& c) { x -= v.x; y -= v.y; return *this; }
-	//Color& operator *= (const Color& c) { x *= v.x; y *= v.y; return *this; }
-	//Color& operator /= (const Color& c) { x /= v.x; y /= v.y; return *this; }
-	//
-	//Color& operator += (float s) { x += s; y += s; return *this; }
-	//Color& operator -= (float s) { x -= s; y -= s; return *this; }
-	//Color& operator *= (float s) { x *= s; y *= s; return *this; }
-	//Color& operator /= (float s) { x /= s; y /= s; return *this; }
+	Color operator + (float s) const { return Color{ r + s, g + s, b + s, a + s }; }
+	Color operator - (float s) const { return Color{ r - s, g - s, b - s, a - s }; }
+	Color operator * (float s) const { return Color{ r * s, g * s, b * s, a * s }; }
+	Color operator / (float s) const { return Color{ r / s, g / s, b / s, a / s }; }
+	
+	Color& operator += (const Color& c) { r += c.r; g += c.g; b += c.b; a += c.a; return *this; }
+	Color& operator -= (const Color& c) { r -= c.r; g -= c.g; b += c.b; a += c.a; return *this; }
+	Color& operator *= (const Color& c) { r *= c.r; g *= c.g; b += c.b; a += c.a; return *this; }
+	Color& operator /= (const Color& c) { r /= c.r; g /= c.g; b += c.b; a += c.a; return *this; }
+	
+	Color& operator += (float s) { r += s; g += s; b += s; a += s; return *this; }
+	Color& operator -= (float s) { r -= s; g -= s; b -= s; a -= s; return *this; }
+	Color& operator *= (float s) { r *= s; g *= s; b *= s; a *= s; return *this; }
+	Color& operator /= (float s) { r /= s; g /= s; b /= s; a /= s; return *this; }
 
 	static uint8_t ToInt(float value) { return static_cast<uint8_t>(Math::Clamp(value, 0.0f, 1.0f) * 255); }
-
 };
 
